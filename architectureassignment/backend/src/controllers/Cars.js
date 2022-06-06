58b9fd49ba8f4d01c336a53146afa7512d463d45
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-06-06 23:10:53 +0530

    added frontend

--- architectureassignment/backend/src/controllers/Cars.js
+++ architectureassignment/backend/src/controllers/Cars.js
@@ -8,15 +8,27 @@ router.get("/", async (req, res) => {
   try {
     const page = req.query.page || 1;
     const pagesize = req.query.pagesize || 10; 
-    const skip = (page - 1) * pagesize; 
-    const filter = req.query.filter || "";
-    const sort = req.query.sort || "";
-    const cars = await Car.find({ "year": {$eq : filter} }).sort({sort:-1})
+    const skip = (page - 1) * pagesize;
+    if(req.query.filter || req.query.sort) {
+      const model = req.query.model || ""; 
+      const filter = req.query.filter || "";
+      const sort = req.query.sort || "";
+      var cars = await Car.find({"company_name": filter}).sort({sort:-1})
       .skip(skip) 
       .limit(pagesize) 
       .lean()
       .exec();
 
+    }else {
+      var cars = await Car.find()
+      .skip(skip) 
+      .limit(pagesize) 
+      .lean()
+      .exec();
+
+    }
+  
+   
     const totalPages = Math.ceil((await Car.find().countDocuments()) / pagesize
     );
 
--- architectureassignment/frontend/src/components/Data.js
+++ architectureassignment/frontend/src/components/Data.js
@@ -1,20 +1,50 @@
 import React from "react";
+import {useEffect, useState} from 'react';
 
+import axios from "axios";
 const Data = () => {
+
+    const [car, setCar] = useState([]);
+    const [page, setPage] = useState(1);
+    const [sort, setSort] = useState("");
+    const [fill, setFill] = useState("");
+    const [ttlpages, setTtlPages] = useState();
+    // let {page, sort, fill} = useParams();
+    
+    const fetchdata = async () => {
+      axios.get(`http://localhost:5000/cars?page=${page}`).then((res) => {setCar(res.data.cars); setTtlPages(res.data.totalPages)});
+    }
+    useEffect(() => {
+       fetchdata();
+       
+    }, [page]);
+    
+    const handlePrevClick = () => {
+      setPage(page-1);
+      fetchdata();
+      
+  }
+
+  const handleNextClick = () => {
+      setPage(page+1);
+      fetchdata();
+  }
   return (
-    <div>
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            <div className="xl:w-1/3 md:w-1/2 p-4">
+      
+    <div >
+       
+             
+       <div className="container px-5 py-16 mx-auto">
+      <div className="flex flex-wrap -m-4">
+      {car.map((e) => { return  <div key={e.car_no} className="xl:w-1/5 md:w-1/2 p-2">
               <div className="border border-gray-200 p-6 rounded-lg">
                 <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
                   <svg
                     fill="none"
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     className="w-6 h-6"
                     viewBox="0 0 24 24"
                   >
@@ -22,18 +52,48 @@ const Data = () => {
                   </svg>
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
-                  Shooting Stars
+                  {e.model_name}
                 </h2>
                 <p className="leading-relaxed text-base">
-                  Fingerstache flexitarian street art 8-bit waist co, subway
-                  tile poke farm.
+                  {e.company_name}
+                </p>
+                <p className="leading-relaxed text-base">
+                  {e.year} - Model
+                </p>
+                <p className="leading-relaxed text-base">
+                  {e.price}
                 </p>
               </div>
-            </div>
+            </div>})}
           </div>
         </div>
-      </section>
-    </div>
+
+        {/*footer*/}
+        <div className="container flex justify-center mx-auto">
+          
+            <div className="flex flex-row mx-auto">
+              <button disabled={page <= 1} onClick={handlePrevClick} type="button" className="bg-gray-800 text-white rounded-l-md border-r border-gray-100 py-2 hover:bg-blue-700 hover:text-white px-3">
+                <div className="flex flex-row align-middle">
+                  <svg className="w-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd"></path>
+                  </svg>
+                  <p className="ml-2">Prev</p>
+                </div>
+              </button>
+              <button disabled={page + 1 > ttlpages} onClick={handleNextClick} type="button" className="bg-gray-800 text-white rounded-r-md py-2 border-l border-gray-200 hover:bg-blue-700 hover:text-white px-3">
+                <div className="flex flex-row align-middle">
+                  <span className="mr-2">Next</span>
+                  <svg className="w-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                  </svg>
+                </div>
+              </button>
+            </div>
+        </div>
+       
+        
+      </div>
+    
   );
 };
 
